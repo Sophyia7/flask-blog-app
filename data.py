b12609d99feefab1e660c1bd63d64cf810a5f454
Sophia Iroegbu <iroegbusophia3@gmail.com>
2022-01-26 23:09:11 +0100

    Added a dummy data file for articles

---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [       
+        {
+            'id': 1, 
+            'title': 'First article',
+            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
+            'author': 'Sophia Iroegbu',
+            'date_created': '26-01-2022'
+        },
+        {
+            'id': 2, 
+            'title': 'Second article',
+            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
+            'author': 'John Doe',
+            'date_created': '25-01-2022'
+        },
+        {
+            'id': 3, 
+            'title': 'Third article',
+            'body': 'lorem ipsum adhdyj gdh gsshh hu gdh gsh asgasd sdivvvvFVUvjusd cgibsdjhbcu',
+            'author': 'Mary James',
+            'date_created': '26-01-2022'
+        },
+    ]
+   
+
+    return articles
