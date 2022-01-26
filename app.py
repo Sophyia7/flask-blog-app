0dd90a99906557135f952ce357ca22ec2986fada
Sophia Iroegbu <iroegbusophia3@gmail.com>
2022-01-26 23:14:19 +0100

    Updated entry file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
@@ -9,6 +12,14 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def display_article(id):
+    return render_template('article.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
